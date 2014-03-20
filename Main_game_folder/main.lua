e661059cb2a6ed8fc86bcee277e696ccd7d3e3c3
WhiteHusky <WhiterSuburban@gmail.com>
2014-03-20 10:48:26 -0400

    Making changes. EVERYWHERE.
    Beep!

--- Main_game_folder/main.lua
+++ Main_game_folder/main.lua
@@ -1,4 +1,5 @@
 function love.draw()
     love.graphics.print("Hello World", 400, 300)
 	love.graphics.print("Husky Likes butts!", 400, 350)
+	love.graphics.print("I can't believe it's not butts!", 400, 375)
 end
