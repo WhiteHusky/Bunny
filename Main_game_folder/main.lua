ba764294c332dd9776a64842e9b631616375f7d9
WhiteHusky <WhiterSuburban@gmail.com>
2014-03-20 10:52:48 -0400

    Trying to fix things Help

--- Main_game_folder/main.lua
+++ Main_game_folder/main.lua
@@ -1,5 +1,9 @@
 function love.draw()
     love.graphics.print("Hello World", 400, 300)
 	love.graphics.print("Husky Likes butts!", 400, 350)
+<<<<<<< HEAD
 	love.graphics.print("I can't believe it's not butts!", 400, 375)
+=======
+	love.graphics.circle( "fill", 300, 300, 50, 100 )
+>>>>>>> 573f14b26881fcf611b66a917828ce73624ab109
 end
