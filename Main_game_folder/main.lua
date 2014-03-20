44023dcc6bf5ab8f8a49bb3bc2a455ff9841e5a5
WhiteHusky <WhiterSuburban@gmail.com>
2014-03-20 11:51:46 -0400

    Still learning.

--- Main_game_folder/main.lua
+++ Main_game_folder/main.lua
@@ -1,12 +1,27 @@
+-- tutorial #1
+-- the 3 golden love function, lua tables, keyboard input, draw circle
+
 function love.load()
-   white = love.graphics.newImage("whitefusionv2avatar.png")
-   x = 50
-   y = 50
+ hero = {} -- new table for the hero
+ hero.x = 300    -- x,y coordinates of the hero
+ hero.y = 450
+ hero.speed = 100
 end
+
+function love.update(dt)
+ if love.keyboard.isDown("left") then
+   hero.x = hero.x - hero.speed*dt
+ elseif love.keyboard.isDown("right") then
+   hero.x = hero.x + hero.speed*dt
+ end
+end
+
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
-	love.graphics.print("Husky Likes butts!", 400, 350)
-	love.graphics.print("I can't believe it's not butts!", 400, 375)
-	love.graphics.circle( "fill", 300, 300, 50, 100 )
-	love.graphics.draw(white, x, y)
+ -- let's draw some ground
+ love.graphics.setColor(0,255,0,255)
+ love.graphics.rectangle("fill", 0,465,800,150)
+
+ -- let's draw our hero
+ love.graphics.setColor(255,255,0,255)
+ love.graphics.rectangle("fill", hero.x,hero.y, 30,15)
 end
